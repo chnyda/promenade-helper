e779e05313aa921e309c4fa6d5c1828e2e5a5205
chnyda <ced.hnyda@gmail.com>
2018-11-15 09:36:23 +0100

    Add script.enabled

--- template/script.sh
+++ template/script.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 {% for node_name, node_ip in nodes %}
-{% if script is defined %}
+{% if script.enabled and script.key_file is defined %}
 scp -i {{ script.key_file }} -o "StrictHostKeyChecking no" join-{{ node_name }}.sh {{ script.user }}@{{ node_ip }}:/home/{{ script.user }}
+{% elif script.enabled %}
+scp -o "StrictHostKeyChecking no" join-{{ node_name }}.sh {{ script.user }}@{{ node_ip }}:/home/{{ script.user }}
 {% else %}
 scp -o "StrictHostKeyChecking no" join-{{ node_name }}.sh {{ node_ip }}:/home/root
 {% endif %}
